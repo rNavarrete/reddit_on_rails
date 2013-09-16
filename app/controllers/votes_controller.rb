38154f6a0423211512cee82c82edc266d2ba8bb7
Edgard R. Navarrete <kinglouis.belmont@gmail.com>
2013-09-16 11:22:35 -0700

    added voting restrictions

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -2,8 +2,14 @@ class VotesController < ApplicationController
   before_filter :authenticate_user!
 
   def create
-    @vote = current_user.votes.create(params[:vote])
-    redirect_to :back
+    @vote = Vote.where(:link_id => params[:vote][:link_id], :user_id => current_user.id).first
+    if @vote
+       @vote.up = params[:vote][:up]
+       @vote.save
+    else
+       @vote = current_user.votes.create(params[:vote])
+    end
+       redirect_to :back
   end
 end  
 
